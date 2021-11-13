3e7f07d8271bdf5344456d0b15bcf4499ac2703e
TheBroz <thebrozyt@outlook.com>
2021-11-12 23:39:14 -0500

    demo moment

--- source/Achievement.hx
+++ source/Achievement.hx
@@ -15,12 +15,12 @@ class Achievement extends FlxSpriteGroup {
     public function new(displayName:String,displayDesc:String){
 		// THIS IS GARBAGE. IM SORRY.
 		super();
-		var daDogDoing:FlxSprite = new FlxSprite(-100,50);
+		var daDogDoing:FlxSprite = new FlxSprite(0,50);
 		var icon:AchievementIcons = new AchievementIcons("icons");
-		var name:FlxText = new FlxText(-100,50,0,displayName);
+		var name:FlxText = new FlxText(0,50,0,displayName);
 		trace(displayName);
 		name.setFormat("VCR OSD Mono", 17, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		var desc:FlxText = new FlxText(-100,50,30,displayDesc);
+		var desc:FlxText = new FlxText(0,50,30,displayDesc);
 		desc.setFormat("VCR OSD Mono", 17, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		icon.animation.curAnim.curFrame = 1;
 		daDogDoing.makeGraphic(120,60,FlxColor.GRAY);
@@ -29,10 +29,10 @@ class Achievement extends FlxSpriteGroup {
 		add(name);
 		add(desc);
 		new FlxTimer().start(1,function(timer:FlxTimer){
-			FlxTween.tween(daDogDoing, {x: FlxG.width + 20},2, {ease: FlxEase.bounceIn});
-			FlxTween.tween(icon, {x: FlxG.width + 20},2, {ease: FlxEase.bounceIn});
-			FlxTween.tween(name, {x: FlxG.width + 30},2, {ease: FlxEase.bounceIn});
-			FlxTween.tween(desc, {x: FlxG.width + 30},2, {ease: FlxEase.bounceIn});
+			FlxTween.tween(daDogDoing, {x: FlxG.width + 300},2, {ease: FlxEase.bounceIn});
+			FlxTween.tween(icon, {x: FlxG.width + 290},2, {ease: FlxEase.bounceIn});
+			FlxTween.tween(name, {x: FlxG.width + 300},2, {ease: FlxEase.bounceIn});
+			FlxTween.tween(desc, {x: FlxG.width + 330},2, {ease: FlxEase.bounceIn});
             new FlxTimer().start(4, function(timer:FlxTimer)
                 {
                     remove(daDogDoing);
@@ -40,7 +40,8 @@ class Achievement extends FlxSpriteGroup {
                     remove(name);
                     remove(desc);
                 });
-		},1000);
+		});	
+		
 	}
 	override function update(elapsed:Float)
 		{
